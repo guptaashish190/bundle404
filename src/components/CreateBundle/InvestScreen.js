34d62cd68bf50f0440b5fb7d82aea31bbffcdfe0
Ashish Gupta <guptaashish190@gmail.com>
2018-10-28 10:32:42 +0530

    merge3

--- src/components/CreateBundle/InvestScreen.js
+++ src/components/CreateBundle/InvestScreen.js
@@ -25,12 +25,14 @@ class Invest extends React.Component {
           <button onClick={() => this.onSubmit()}>Invest</button>
           {
               this.state.success ?
-                <div className="check_mark">
-                  <div className="sa-icon sa-success animate">
-                    <span className="sa-line sa-tip animateSuccessTip" />
-                    <span className="sa-line sa-long animateSuccessLong" />
-                    <div className="sa-placeholder" />
-                    <div className="sa-fix" />
+                <div className="check-cont">
+                  <div className="check_mark">
+                    <div className="sa-icon sa-success animate">
+                      <span className="sa-line sa-tip animateSuccessTip" />
+                      <span className="sa-line sa-long animateSuccessLong" />
+                      <div className="sa-placeholder" />
+                      <div className="sa-fix" />
+                    </div>
                   </div>
                 </div> :
           ''
